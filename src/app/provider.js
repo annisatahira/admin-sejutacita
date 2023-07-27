75a3c9881bf2fe76639cd8f4eed740ed962d0b72
annisatahira <annisatahira.jsc@gmail.com>
2023-07-27 21:02:19 +0700

    feat: add landing page

--- src/app/provider.js
+++ src/app/provider.js
@@ -1,11 +1,8 @@
-'use client';
+"use client";
 
+// import { CartContext } from "@/context/cartContext";
 import { ProductProvider } from "@/context/productContext";
 
 export function Providers({ children }) {
-    return (
-        <ProductProvider>
-            {children}
-        </ProductProvider>
-    );
+  return <ProductProvider>{children}</ProductProvider>;
 }
