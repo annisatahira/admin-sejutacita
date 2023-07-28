e3fdcbdc973828008e53ce5c87404c3fff7c3e5a
annisatahira <annisatahira.jsc@gmail.com>
2023-07-28 11:52:06 +0700

    feat: responsive cart pages

--- src/app/dashboard/carts/[id]/page.js
+++ src/app/dashboard/carts/[id]/page.js
@@ -36,16 +36,23 @@ const CartDetail = () => {
       />
       <div className="w-full my-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         {!loading ? (
-          <ul className="grid grid-cols-2 leading-10 mb-3">
-            <li>Username : {user?.username}</li>
+          <ul className="grid md:grid-cols-2 leading-10 mb-3">
             <li>
-              {cartProducts?.totalProducts} of item{" "}
-              {cartProducts?.totalQuantity}
+              <span className="font-bold">Username</span> : {user?.username}
             </li>
             <li>
-              Fullname : {user?.firstName} {user?.lastName}
+              <span className="font-bold">{cartProducts?.totalProducts}</span>{" "}
+              of item{" "}
+              <span className="font-bold">{cartProducts?.totalQuantity}</span>
             </li>
-            <li>Total Amount : {cartProducts?.total}</li>
+            <li>
+              <span className="font-bold">Fullname</span> : {user?.firstName}{" "}
+              {user?.lastName}
+            </li>
+            <li>
+              <span className="font-bold">Total Amount</span> :{" "}
+              {cartProducts?.total}
+            </li>
           </ul>
         ) : (
           <Loading />
